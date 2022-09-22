485ac59419a250d64096dceb85854c4d935dbd96
AmitKumar-AK <amitkumar_mca04@hotmail.com>
2022-09-23 00:06:23 +0100

    Changes related to custom model deployment added.

---
+++ src/Environment/xconnect/Model/code/Models/Preference/AreaOfInterest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CT.Environment.XConnect.Model.Models.Preference
+{
+    public class AreaOfInterest
+    {
+        public string AreaOfInterestId { get; set; }
+        public string AreaOfInterestName { get; set; }
+    }
+}
---
+++ src/Environment/xconnect/Model/code/Models/Preference/Communication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CT.Environment.XConnect.Model.Models.Preference
+{
+    public class Communication
+    {
+        public string CommunicationId { get; set; }
+        public string CommunicationName { get; set; }
+    }
+}
